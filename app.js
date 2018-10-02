a855eba398aa78e2a791fad9d6e9575449191b9e
Rose Wanjohi <rwwanjohi10@gmail.com>
2018-10-02 11:27:38 +0000

    env_var

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var campgroundRoutes = require("./routers/campground"),
     
 // seedDB();
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true }); 
-mongoose.connect("mongodb://rose:rose12345@ds119993.mlab.com:19993/yelpcamp", { useNewUrlParser: true });
+mongoose.connect(process.env.localDB, { useNewUrlParser: true }); 
+// mongoose.connect("mongodb://rose:rose12345@ds119993.mlab.com:19993/yelpcamp");
 mongoose.set("useFindAndModify", false);
 
 // APP CONFIG
